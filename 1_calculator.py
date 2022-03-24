f388490ba94a6cf40351abd0a63a917779e61dd1
Diana <diana.pronina6c@gmail.com>
2022-03-24 10:53:46 +0200

    calculator

---
+++ 1_calculator.py
@@ -0,0 +1,20 @@
+operation = input("Please write + or - or * or / to pick calculator: ")
+if operation == "+":
+    number1 = int(input("Please enter nr1: "))
+    number2 = int(input("Please enter nr2: "))
+    print(number1 + number2)
+
+if operation == "-":
+    number1 = int(input("Please enter nr1: "))
+    number2 = int(input("Please enter nr2: "))
+    print(number1 - number2)
+
+if operation == "*":
+    number1 = int(input("Please enter nr1: "))
+    number2 = int(input("Please enter nr2: "))
+    print(number1 * number2)
+
+if operation == "/":
+    number1 = int(input("Please enter nr1: "))
+    number2 = int(input("Please enter nr2: "))
+    print(number1 / number2)
