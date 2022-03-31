e2ca075a3974c10975a99da1777b4e4c5ed93db7
wxwvvxwvxvwvxv <diana.pronina6c@gmail.com>
2022-03-31 07:55:19 +0300

    Update 2_tictactoe.py

--- 2_tictactoe.py
+++ 2_tictactoe.py
@@ -1,85 +1,88 @@
-next_position = input("Please type what position: ")
+game = [
+    ".", ".", ".",
+    ".", ".", ".",
+    ".", ".", "."
+]
+def gamePlan():
+    print(game[0] + game[1] + game[2])
+    print(game[3] + game[4] + game[5])
+    print(game[6] + game[7] + game[8])
 
-if next_position == "1":
-    print("x . .")
-    print(". . .")
-    print(". . .")
+for i in range(1, 10):
+    if i % 2 != 0:
+        next_position = int(input("(Player one) Type what position: "))
+        if game[next_position - 1] == "x":
+            print("You typed wrong position")
+            break
 
-    next_position = input("Please type what position: ")
+        elif game[next_position - 1] == "o":
+            print("You typed wrong position")
+            break
 
-    if next_position == "1":
-        print("CANT DO THE MOVE")
+        else:
+            game[next_position - 1] = "x"
+            gamePlan()
+        
+    else:
+        next_position = int(input("(Player two) Type what position: "))
+        if game[next_position - 1] == "x":
+            print("You typed wrong position")
+            break
+        
+        elif game[next_position - 1] == "o":
+            print("You typed wrong position")
+            break
 
-    elif next_position == "2":
-        print("x o .")
-        print(". . .")
-        print(". . .")
+        else:
+            game[next_position - 1] = "o"
+            gamePlan()
 
-        next_position = input("Please type what position: ")
+    if game[0] == "x"and game[3] == "x" and game[6] == "x":
+        print("Player one - winner")
+        break
+    elif game[1] == "x" and game[4] == "x" and game[7] == "x":
+        print("Player one - winner")
+        break
+    elif game[2] == "x" and game[5] == "x" and game[8] == "x":
+        print("Player one - winner")
+        break
+    elif game[0] == "x" and game[1] == "x" and game[2] == "x":
+        print("Player one - winner")
+        break
+    elif game[3] == "x" and game[4] == "x" and game[5] == "x":
+        print("Player one - winner")
+        break
+    elif game[6] == "x" and game[7] == "x" and game[8] == "x":
+        print("Player one - winner")
+        break
+    elif game[0] == "x" and game[4] == "x" and game[8] == "x":
+        print("Player one - winner")
+        break
+    elif game[3] == "x" and game[4] == "x" and game[6] == "x":
+        print("Player one - winner")
+        break
 
-        if next_position == "1":
-            print("CANT DO THE MOVE")
-
-        elif next_position == "2":
-            print("CANT DO THE MOVE")
-
-        elif next_position == "3":
-            print("x o x")
-            print(". . .")
-            print(". . .")
-
-            next_position = input("Please type what position: ")
-            
-            if next_position == "1":
-                print("CANT DO THE MOVE")
-            
-            elif next_position == "2":
-                print("CANT DO THE MOVE")
-            
-            elif next_position == "3":
-                print("CANT DO THE MOVE")
-            
-            elif next_position == "4":
-                print("x o x")
-                print("o . .")
-                print(". . .")
-
-
-
-
-
-elif next_position == "3":
-    print(". . x")
-    print(". . .")
-    print(". . .")
-
-elif next_position == "4":
-    print(". . .")
-    print("x . .")
-    print(". . .")
-
-
-if next_position == "5":
-    print(". . .")
-    print(". x .")
-    print(". . .")
-
-if next_position == "6":
-    print(". . .")
-    print(". . x")
-    print(". . .")
-
-if next_position == "7":
-    print(". . .")
-    print(". . .")
-    print("x . .")
-
-if next_position == "8":
-    print(". . .")
-    print(". . .")
-    print(". x .")
-
-if next_position == "9":
-    print(". . .")
-    print(". . .")
-    print(". . x")
+    if game[0] == "o" and game[3] == "o" and game[6] == "o":
+        print("Player two - winner")
+        break
+    elif game[1] == "o" and game[4] == "o" and game[7] == "o":
+        print("Player two - winner")
+        break
+    elif game[2] == "o" and game[5] == "o" and game[8] == "o":
+        print("Player two - winner")
+        break
+    elif game[0] == "o" and game[1] == "o" and game[2] == "o":
+        print("Player two - winner")
+        break
+    elif game[3] == "o" and game[4] == "o" and game[5] == "o":
+        print("Player two - winner")
+        break
+    elif game[6] == "o" and game[7] == "o" and game[8] == "o":
+        print("Player two - winner")
+        break
+    elif game[0] == "o" and game[4] == "o" and game[8] == "o":
+        print("Player two - winner")
+        break
+    elif game[3] == "o" and game[4] == "o" and game[6] == "o":
+        print("Player two - winner")
+        break
