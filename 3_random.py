464e82c9cc4b2070d71429cadf68838a4fa53235
Diana <diana.pronina6c@gmail.com>
2022-03-31 11:08:31 +0300

    Create 3_random.py

---
+++ 3_random.py
@@ -0,0 +1,13 @@
+import random
+
+random_number = random.random()
+print("Random number is: ", random_number)
+
+if random_number < 0.25:
+    print("try to go right")
+elif random_number > 0.25 and random_number < 0.5:
+    print("try to go left")
+elif random_number > 0.5 and random_number < 0.75:
+    print("try to go top")
+else:
+    print("try to go bottom")
