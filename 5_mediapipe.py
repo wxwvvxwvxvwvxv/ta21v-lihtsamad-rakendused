203ff35858d50724a6db6b06b7101f8f9adc8099
Diana <diana.pronina6c@gmail.com>
2022-04-21 10:44:45 +0300

    Update 5_mediapipe.py

--- 5_mediapipe.py
+++ 5_mediapipe.py
@@ -1,4 +1,3 @@
-from tkinter import image_names
 import cv2
 import mediapipe as mp
 mp_drawing = mp.solutions.drawing_utils
@@ -24,26 +23,34 @@ with mp_hands.Hands(
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = hands.process(image)
 
-
-
     # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    left_hand_x = 1
+    right_hand_x = 1
+    left_hand_y = 1
+    right_hand_y = 1
     if results.multi_hand_landmarks:
       for hand_landmarks in results.multi_hand_landmarks:
-        print(hand_landmarks.landmark[8])
-        image_height, image_width, _ = image.shape
         x = int(hand_landmarks.landmark[8].x * 640)
         y = int(hand_landmarks.landmark[8].y * 480)
+        if x < 480/2:
+          left_hand_y = y
+          left_hand_x = x
+        else:
+          right_hand_y = y
+          right_hand_x = x
         
         center_coordinates = (x, y)
         radius = 50
         color = (255, 0, 0)
-        thickness = 2
-        cv2.circle(image, center_coordinates, radius, color, thickness)
+       
 
-        
-        cv2.rectangle(image, (20,y),(20, y+50), color, thickness)
+        cv2.circle(image, (left_hand_x, left_hand_y), 10, color, 2)
+        cv2.rectangle(image, (20,left_hand_y),(20, left_hand_y+50), (0,255,0), 3)
+
+        cv2.circle(image, (right_hand_x, right_hand_y), 10, color, 2)
+        cv2.rectangle(image, (460, right_hand_y),(460, right_hand_y+50), (0,255,0), 3)
         
     # Flip the image horizontally for a selfie-view display.
     cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
